41f0ca0cf76319eb1920e0826766ceb408279175
RadouaneAbn <radouaneabn@gmail.com>
2026-09-10 13:33:12 +0200

    done

--- inception/srcs/requirements/wordpress/tools/entrypoint.sh
+++ inception/srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,25 +1,13 @@
 #!/bin/bash
 set -e
 
+chown -R www-data:www-data /var/www/wordpress
+chmod -R 775 /var/www/wordpress
+
 if [ ! -f /var/www/wordpress/wp-config.php ]; then
     DB_USER_PASS=$(cat /run/secrets/db_user_pass)
     WP_USER_PASS=$(cat /run/secrets/wp_user_pass)
     WP_ADMIN_PASS=$(cat /run/secrets/wp_admin_pass)
-
-    echo "DB_NAME: $DB_NAME"
-    echo "DB_USER: $DB_USER"
-    echo "DB_USER_PASS: $DB_USER_PASS"
-    echo "DB_PORT: $DB_PORT"
-
-    echo "DOMAIN_NAME: $DOMAIN_NAME"
-    echo "WP_ADMIN_USER: $WP_ADMIN_USER"
-    echo "WP_ADMIN_EMAIL: $WP_ADMIN_EMAIL"
-    echo "WP_ADMIN_PASS: $WP_ADMIN_PASS"
-
-    echo "WP_USER: $WP_USER"
-    echo "WP_USER_EMAIL: $WP_USER_EMAIL"
-    echo "WP_USER_PASS: $WP_USER_PASS"
-
 
     wp core download --allow-root --path=/var/www/wordpress
 
@@ -47,8 +35,5 @@ if [ ! -f /var/www/wordpress/wp-config.php ]; then
         --role=author \
         --path=/var/www/wordpress
 fi
-
-chown -R www-data:www-data /var/www/wordpress
-chmod -R 775 /var/www/wordpress
 
 exec "$@"
