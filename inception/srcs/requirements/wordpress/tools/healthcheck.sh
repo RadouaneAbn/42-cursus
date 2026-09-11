88847e04d1eab9ec91bfc9bb843d1dd1ad21e175
RadouaneAbn <radouaneabn@gmail.com>
2026-09-11 16:16:27 +0200

    simpler Makefile make the wordpress port more flexible in the healthchek

--- inception/srcs/requirements/wordpress/tools/healthcheck.sh
+++ inception/srcs/requirements/wordpress/tools/healthcheck.sh
@@ -4,7 +4,7 @@ echo '<?php echo "OK"; ?>' > /tmp/health.php
 
 if SCRIPT_FILENAME=/tmp/health.php \
    REQUEST_METHOD=GET \
-   cgi-fcgi -bind -connect 127.0.0.1:9000 \
+   cgi-fcgi -bind -connect 127.0.0.1:${WORDPRESS_PORT} \
    | grep -q "OK"
 then
     exit 0
